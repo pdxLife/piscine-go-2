c144d7edb71a0c6ead78c24aaa303d618852a857
condexter <student@ALEM-7de69b.alem.school>
2019-10-16 11:29:49 +0600

    q2t1

---
+++ printalphabet/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+        "fmt"
+        "unicode"
+)
+func  loweralpha() string {
+       p :=make([]byte, 26)
+       for i := range p {
+              p[i] = 'a' + byte(i)
+       }
+       return string(p)
+}
