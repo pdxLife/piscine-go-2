42a74065c4b95570fe88153b03fc5f1d03d1a45a
condexter <46655031+condexter@users.noreply.github.com>
2019-10-16 17:47:29 +0600

    q2t3

--- printdigits/main.go
+++ printdigits/main.go
@@ -3,11 +3,11 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	var aRune rune = 48
-	for aRune <= 57 {
+	var fuck rune = 48
+	for fuck <= 57 {
 
-		z01.PrintRune(aRune)
-		aRune++
+		z01.PrintRune(fuck)
+		fuck++
 	}
 	z01.PrintRune(10)
 }
