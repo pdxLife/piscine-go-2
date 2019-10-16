67a62cdd815d34bf7f7c4b7ff22ea73df878946a
condexter <student@ALEM-7de69b.alem.school>
2019-10-16 12:52:00 +0600

    q2t1

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-       "github.com/01-edu/z01"
+	"github.com/01-edu/z01"
 )
 
 func main() {
-        i := 97
-        for i <= 122 {
-                z01.PrintRune(rune(i))
-                i++
-        }
-<<<<<<< HEAD
-        
-        z01.PrintRune('\n')
+	i := 97
+	for i <= 122 {
+		z01.PrintRune(rune(i))
+		i++
+	}
+	z01.PrintRune('\n')
 }
-=======
-       
-        z01.PrintRune('\n)
-}
->>>>>>> 76af816d1e2f362b4f671b5bf77dfdf7d9b3abfa
