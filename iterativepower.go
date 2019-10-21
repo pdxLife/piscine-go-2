4e183bab2f6b93047923fe6cb2191e626d894e1a
condexter <student@ALEM-7de69b.alem.school>
2019-10-21 15:47:20 +0600

    q4t3

--- iterativepower.go
+++ iterativepower.go
@@ -1,13 +1,16 @@
 package piscine
 
 func IterativePower(nb int, power int) int {
-	if nb < 20 && nb > -20 && power < 20 && power > 0 {
+	if nb > 20 || nb < -20 || power < 0 {
+		return 0
+	}
+	if power == 0 {
+		return 1
+	} else {
 		result := 1
 		for l := 1; l <= power; l++ {
 			result = result * nb
 		}
 		return result
-	} else {
-		return 1
 	}
 }
