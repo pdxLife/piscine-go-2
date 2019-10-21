866768c74e91c2fb8b9194ab56ce251d1cea72ca
condexter <student@ALEM-7de69b.alem.school>
2019-10-21 13:53:16 +0600

    q4t3

--- iterativepower.go
+++ iterativepower.go
@@ -4,7 +4,7 @@ func IterativePower(nb int, power int) int {
 	if nb < 20 && nb > -1 {
 		result := 1
 		for l := 1; l <= power; l++ {
-			result *= nb
+			result = result * nb
 		}
 		return result
 	} else {
