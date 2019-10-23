e56ff63f91c47f55fa3db9574a85bf01204abef8
condexter <student@ALEM-7de69b.alem.school>
2019-10-23 15:25:54 +0600

    eh

--- index.go
+++ index.go
@@ -1,12 +1,34 @@
 package piscine
 
 func Index(s string, toFind string) int {
-
-	for i := 0; i < len(s)-len(toFind); i++ {
-
-		if s[i:i+len(toFind)] == toFind {
-			return i
+	if toFind == "" {
+		return 0
+	}
+	size := 0
+	for range toFind {
+		size++
+	}
+	b := -1
+	count := 0
+	for i := range s {
+		if count == size {
+			break
+		} else if s[i] == toFind[count] {
+			if count == 0 {
+				b = i
+			}
+			count++
+		} else {
+			count = 0
+			b = -1
 		}
 	}
-	return -1
+	if count != size {
+		return -1
+	}
+	if b == -1 {
+		return -1
+	}
+	return b
+
 }
