863cb533a699f306ba515ba848220c33269ffc34
pdxLife <44197020+pdxLife@users.noreply.github.com>
2019-10-30 13:08:03 +0600

    Update main.go

--- printparams/main.go
+++ printparams/main.go
@@ -9,8 +9,8 @@ func main() {
 	name := os.Args
 	for j := range name {
 		if j > 0 {
-			a := []rune(name[j])
-			for i := range name[j] {
+			a := []rune(name[j]) // создаешь слайс из рун с элементами j которые сейчас находятся в цикле 
+			for i := range name[j] { //цикл создается, 
 				z01.PrintRune(a[i])
 			}
 			z01.PrintRune(10)
